5bb58f3c1d08b7cbd030cc169197da99ad723ad7
abinofbrooklyn <Your email address>
2014-05-30 22:36:41 -0400

    add ch5 exercises

---
+++ LTP/ch5.rb
@@ -0,0 +1,10 @@
+puts "What do you want?"
+want = gets.chomp
+puts "WHADDAYA MEAN" " I WANT " + want.upcase + " ?!? YOU\'RE FIRED!!"
+
+page_width = 50
+puts ("Table of Contents".center(page_width))
+puts ""
+puts ("Chapter 1:  Numbers".ljust(page_width/2) + "page 1".rjust(page_width/2))
+puts ("Chapter 2:  Letters".ljust(page_width/2) + "page 72".rjust(page_width/2))
+puts ("Chapter 3:  Variables".ljust(page_width/2) + "page 118".rjust(page_width/2)) 
